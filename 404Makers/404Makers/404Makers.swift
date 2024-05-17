681d9a7141977cb92ae6900c14221387dda96726
Owen Kim <godkwk5@naver.com>
2024-05-17 20:57:09 +0900

    set

---
+++ 404Makers/404Makers/404Makers.swift
@@ -0,0 +1,17 @@
+//
+//  404Makers.swift
+//  404Makers
+//
+//  Created by 김우경 on 5/17/24.
+//
+
+import SwiftUI
+
+@main
+struct MakersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
